9d028c541cd9213644d4ee1a63ba66c012899ca7
sediar0627 <=>
2022-01-24 10:00:34 -0500

    Creacion de componentes

---
+++ app/Http/Livewire/Piscinas.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class Piscinas extends Component
+{
+    public function render()
+    {
+        return view('livewire.piscinas');
+    }
+}
--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -13,7 +13,19 @@
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                     <x-jet-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
+                        {{ __('INICIO') }}
+                    </x-jet-nav-link>
+                </div>
+
+                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                    <x-jet-nav-link href="{{ route('piscinas') }}" :active="request()->routeIs('piscinas')">
+                        {{ __('PISCINAS') }}
+                    </x-jet-nav-link>
+                </div>
+
+                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                    <x-jet-nav-link href="{{ route('lecturas') }}" :active="request()->routeIs('lecturas')">
+                        {{ __('LECTURAS') }}
                     </x-jet-nav-link>
                 </div>
             </div>
@@ -139,7 +151,19 @@
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
             <x-jet-responsive-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
-                {{ __('Dashboard') }}
+                {{ __('INICIO') }}
+            </x-jet-responsive-nav-link>
+        </div>
+
+        <div class="pt-2 pb-3 space-y-1">
+            <x-jet-responsive-nav-link href="{{ route('piscinas') }}" :active="request()->routeIs('piscinas')">
+                {{ __('PISCINAS') }}
+            </x-jet-responsive-nav-link>
+        </div>
+
+        <div class="pt-2 pb-3 space-y-1">
+            <x-jet-responsive-nav-link href="{{ route('lecturas') }}" :active="request()->routeIs('lecturas')">
+                {{ __('LECTURAS') }}
             </x-jet-responsive-nav-link>
         </div>
 
